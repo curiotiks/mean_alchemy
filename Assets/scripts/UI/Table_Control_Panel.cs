124991621ff8b4d51ceb51858a428a8744ed408b
nahunhee <nahunhee@nate.com>
2023-04-03 23:19:45 -0400

    table plot update

--- Assets/scripts/UI/Table_Control_Panel.cs
+++ Assets/scripts/UI/Table_Control_Panel.cs
@@ -8,10 +8,10 @@ using TMPro;
 
 public class Table_Control_Panel : MonoBehaviour
 {
+    public static Table_Control_Panel instance;
     public TextMeshProUGUI mean_text; 
     public TextMeshProUGUI sd_text;
-    public Table_Plot_Panel plot_panel;
-    public static int max_height_count = 10; //how many in a column
+    public Table_Plot_Panel plot_panel; 
     [HideInInspector]
     public List<int> numbers_list {get; set;}
     public double mean;
@@ -20,9 +20,18 @@ public class Table_Control_Panel : MonoBehaviour
 
     void Awake() {
         numbers_list = new List<int>();
+        instance = this;
     }
+    
     public void updateInput(int num, bool isAdded = true){
         if (isAdded){
+            //if the numbers_list contains the certain number more than 10 elements, return
+            if(numbers_list.Count(x => x == num) >= plot_panel.max_stacked_count){
+                Debug.Log("numbers_list contains "+num +"more than "+ plot_panel.max_stacked_count +" elements");
+                return;
+            }
+
+
             Debug.Log("new input: "+num);
             numbers_list.Add( num );
         }else{
@@ -40,8 +49,7 @@ public class Table_Control_Panel : MonoBehaviour
         }catch(InvalidOperationException e){
             mean = 0;
             sd = 0;
-        }
-        
+        } 
 
         Debug.Log("new mean: "+mean);
         Debug.Log("new sd: "+sd);
--- Assets/scripts/UI/Table_Plot_Panel.cs
+++ Assets/scripts/UI/Table_Plot_Panel.cs
@@ -2,44 +2,89 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Table_Plot_Panel : MonoBehaviour
 { 
-    public RectTransform plot_holder; 
-    float chart_height;
-    float an_increment_height;
-    private List<Table_Plot_Item> plot_item_list;
+    private List<VerticalLayoutGroup> vertical_layout_group_list = new List<VerticalLayoutGroup>();
+    public int max_stacked_count = 10;
+    public TextMeshProUGUI y_axis;
 
-    void Start() {
-        chart_height = Screen.height + plot_holder.sizeDelta.y;
-        an_increment_height = chart_height / (float) Table_Control_Panel.max_height_count; 
-        an_increment_height *= 0.8f;
-        //plot bar items --> list
-        plot_item_list = new List<Table_Plot_Item>();
-        foreach(var x in plot_holder.GetComponentsInChildren<Table_Plot_Item>(true)){
-            plot_item_list.Add(x);
+    void Start() { 
+        //find all vertical layout group
+        //vertical layout group is placed in each numbered block
+        y_axis.text = max_stacked_count.ToString();
+        foreach(var x in GetComponentsInChildren<VerticalLayoutGroup>()){
+            vertical_layout_group_list.Add(x);
+        } 
+        //validate if the number of vertical layout group is 10
+        if(vertical_layout_group_list.Count != 10){
+            Debug.Log("vertical layout group count is not 10");
         }
 
+        instantiateVerticalLayoutGroups();
     } 
 
-    //num ranging from 1 to 10
+    public void instantiateVerticalLayoutGroups(){
+        int num_for_name = 1;
+        foreach (VerticalLayoutGroup x in vertical_layout_group_list){
+            //find imge from numbers_panel buttons
+            GameObject img_obj = GameObject.Find("btn_num_" + num_for_name);
+            // Debug.Log("img_obj" + img_obj);
+            
+            //add a new gameobject to the vertical layout group
+            for (int i = 0; i < max_stacked_count; i++){
+                GameObject new_obj = new GameObject();
+                new_obj.name = "componentVerticalLayout_"+num_for_name + "_" + i;
+                new_obj.AddComponent<RectTransform>();
+                new_obj.transform.SetParent(x.transform);
+                new_obj.AddComponent<Image>();
+                new_obj.GetComponent<Image>().sprite = img_obj.GetComponent<Image>().sprite;
+                //set image transparent
+                new_obj.GetComponent<Image>().color = new Color(1,1,1,0);
+                new_obj.AddComponent<Button>();
+                Button btn = new_obj.GetComponent<Button>();
+                btn.interactable = false;
+                btn.onClick.AddListener( () => { 
+                    Table_Control_Panel.instance.updateInput(int.Parse(new_obj.name.Split('_')[1]), false);
+                });
+            }
+            num_for_name++;
+        }
+    } 
+
     public void drawPlot(int num, bool isAdded){
-        RectTransform rt = plot_item_list[num-1].GetComponent<RectTransform>();
-        Table_Plot_Item item = plot_item_list[num-1];
-        float originalSize = rt.sizeDelta.y;
+        // Debug.Log("drawPlot: "+num+" "+isAdded);
+        //find the vertical layout group
+        VerticalLayoutGroup vlg = vertical_layout_group_list[num-1];
+        //if isAdded is true, find the last child which the image is not transparent is disabled and set the image component tranparent and enable the button.
         if(isAdded){
-            rt.sizeDelta = new Vector2(rt.sizeDelta.x, rt.sizeDelta.y + an_increment_height);
-            item.incrementStackedCount();
-        }else{ 
-            rt.sizeDelta = new Vector2(rt.sizeDelta.x, rt.sizeDelta.y - an_increment_height);
-            item.decrementStackedCount();
+            for (int i = vlg.transform.childCount - 1; i >= 0; i--){
+                Transform child = vlg.transform.GetChild(i);
+                if (child.GetComponent<Image>().color.a == 0){
+                    child.GetComponent<Image>().color = new Color(1,1,1,1);
+                    child.GetComponent<Button>().interactable = true;
+                    break;
+                }
+            }
+        }
+        else{
+            //if isAdded is false, find the last child which the image is not transparent and set the image component tranparent and disable the button.
+            for (int i = 0; i < vlg.transform.childCount; i++){
+                Transform child = vlg.transform.GetChild(i);
+                if (child.GetComponent<Image>().color.a != 0){
+                    child.GetComponent<Image>().color = new Color(1,1,1,0);
+                    child.GetComponent<Button>().interactable = false;
+                    break;
+                }
+            }
         }
     }
 
     public void resetPlot(){
-        foreach(var x in plot_item_list){
-            RectTransform rt = x.GetComponent<RectTransform>();
-            rt.sizeDelta = new Vector2(rt.sizeDelta.x, 0);
-        }
+        // foreach(var x in plot_item_list){
+        //     RectTransform rt = x.GetComponent<RectTransform>();
+        //     rt.sizeDelta = new Vector2(rt.sizeDelta.x, 0);
+        // }
     }
 }
