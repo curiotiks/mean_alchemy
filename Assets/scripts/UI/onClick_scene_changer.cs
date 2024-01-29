336b271209dc87e5abd65c857707ad2eaa2a6f56
Curt Fulwider <curiotiks@icould.com>
2024-01-29 12:46:05 -0500

    Trying to add scene changes

---
+++ Assets/scripts/UI/onClick_scene_changer.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class onClick_scene_changer : MonoBehaviour
+{
+    [SerializeField] private string destScene;
+ 
+ private void OnCollisionEnter2D(Collision2D collision) {
+    if (collision.gameObject.name == "Player"){
+        SceneManager.LoadScene(destScene);
+        Debug.Log("Scene Triggered");
+ }
+}
+}
